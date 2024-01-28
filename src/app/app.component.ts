c9302769d3642742a054cca230b578ea3ee48110
Marvin Verdera <verderm@ph.ibm.com>
2024-01-28 14:25:43 +0800

    Add my name in home page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
-  title = 'myDevPortfolio';
+  title = 'Marvin verdera Portfolio';
 }
