d66a5ef69c7b2ceb8f4939bffeac978d163a95fe
Marvin Verdera <verderm@ph.ibm.com>
2024-01-31 00:33:45 +0800

    Navigation and Hero section.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMoon, faSun, faArrowDown } from '@fortawesome/free-solid-svg-icons';
+
+declare var displayThemeButton: any;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'Marvin verdera Portfolio';
+  faMoon = faMoon;
+  faSun = faSun;
+  faArrowDown = faArrowDown;
+
+  ngOnInit(): void {
+    new displayThemeButton();
+  }
 }
+
+
